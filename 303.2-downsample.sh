24a95af998640c850f7fc4edbc5bb2b90bd6661c
Mac Campbell <campbellmatthewa@gmail.com>
2025-11-24 09:03:43 +1100

    Trying to combine Life History with parents

---
+++ 303.2-downsample.sh
@@ -0,0 +1,7 @@
+samtools view -bs 0.228142130265734 data/parents/SRR35397523.sort.flt.bam > data/downsample/SRR35397523.reduced.bam
+samtools view -bs 0.499243147388559 data/parents/SRR35397524.sort.flt.bam > data/downsample/SRR35397524.reduced.bam
+samtools view -bs 0.262147694797692 data/parents/SRR35397549.sort.flt.bam > data/downsample/SRR35397549.reduced.bam
+samtools view -bs 0.243835235654563 data/parents/SRR35397572.sort.flt.bam > data/downsample/SRR35397572.reduced.bam
+samtools view -bs 0.171632834116351 data/parents/SRR35397573.sort.flt.bam > data/downsample/SRR35397573.reduced.bam
+samtools view -bs 0.234339116822737 data/parents/SRR35397583.sort.flt.bam > data/downsample/SRR35397583.reduced.bam
+samtools view -bs 0.10757251328655 data/parents/SRR35397627.sort.flt.bam > data/downsample/SRR35397627.reduced.bam
